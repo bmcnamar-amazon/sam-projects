6d6fabde434c2f326fa7c61b3f5e6d7ffd9d1a0e
Brian McNamara <bmcnamar@amazon.com>
2018-08-02 13:40:01 -0400

    Update to include company images in the reports

--- lambda/jsonToPdf/index.py
+++ lambda/jsonToPdf/index.py
@@ -3,12 +3,14 @@ import json
 import os
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
+from reportlab.lib.utils import ImageReader
 import sys
 
 s3 = boto3.resource('s3')
 
 output_bucket = os.environ['OUTPUT_BUCKET']
 bucket_keyspace = 'report'
+url = 'http://asap-image-tank.s3-website-us-east-1.amazonaws.com'
 
 def handler(event, context):
     input_bucket = event['Records'][0]['s3']['bucket']['name']
@@ -29,6 +31,10 @@ def handler(event, context):
             name = customer['name']
             ticker = customer['ticker']
             link = customer['url']
+            if customer['logo']:
+                image_url = '{}/{}'.format(url, customer['logo'])
+            else:
+                image_url = '{}/{}'.format(url, 'AMZN.jpg')
             print 'Working on {}...'.format(customer['name'])
 
             try:
@@ -36,17 +42,22 @@ def handler(event, context):
                 report_name = '{}.pdf'.format(customer['name'].replace(' ','_'))
                 full_report = report_directory + '/' + report_name
                 print 'Creating report {}'.format(report_name)
+
+                logo = ImageReader(image_url)
+
                 c = canvas.Canvas(full_report, pagesize=letter)
                 c.drawString(100,750,"FINANCIAL REPORT")
                 c.drawString(100,735,"Prepared by: Research Function ({})".format(report_date))
                 c.drawString(100,700,"Customer: {}".format(name))
                 c.drawString(100,685,"Ticker: {}".format(ticker))
                 c.drawString(100,670,"Information URL: {}".format(link))
+                c.drawImage(logo, 500, 670, width=100, height=100, mask='auto')
                 c.save()
                 print 'Saving report {} to s3://{}/{}/{}'.format(report_name, output_bucket, bucket_keyspace, report_name)
                 s3.meta.client.upload_file(full_report, output_bucket, '{}/{}'.format(bucket_keyspace, report_name))
             except Exception as e:
                 print 'FAILURE creating and saving report for {}: {}'.format(customer['name'], str(e))
-                sys.exit(1)
+                continue
+    
     print 'Report generation complete'
     return 'Report generation complete'
